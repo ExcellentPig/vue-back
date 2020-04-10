14b608dac900b5e5400587d9b798dbcde0083af8
ExcellentPig <58382766+ExcellentPig@users.noreply.github.com>
2020-04-10 16:13:03 +0800

    init

---
+++ server.js
@@ -0,0 +1,18 @@
+let http = require('http');
+let users = [
+	{id:1,name:'khan1'},
+	{id:2,name:'khan2'},
+	{id:3,name:'khan3'}
+]
+let server = http.createServer(function(req,res){
+	res.setHeader('Access-Control-Allow-Origin','*');
+	if(req.url === '/api/users'){
+		res.end(JSON.stringify(users));
+	}else{
+		res.end
+		('Not Found');
+	}
+})
+server.listen(3000,()=>{
+	console.log('后端API接口服务器启动在3000端口')
+});
